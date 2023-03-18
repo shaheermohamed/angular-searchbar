c3de85bab0a13432cb0e1b3850c63d73d3f9e434
Mohamed Shaheer <shaheer@coderythm.com>
2023-03-18 23:51:01 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  displayVal:string='';
+  getValue(val:string)
+  {
+    console.warn(val)
+    this.displayVal = val
+  } 
 }
